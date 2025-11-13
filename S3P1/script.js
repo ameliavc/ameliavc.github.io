4d4417ac0ee20ac947f7cd9b921e6cebc615f727
ameliavc <ameliavc@seas.upenn.edu>
2025-11-13 16:16:39 -0500

    Add files via upload

--- S3P1/script.js
+++ S3P1/script.js
@@ -17,137 +17,162 @@ gvalue = 0
 avalue = 0
 
 
-
 // e = 12
 function changeColor() {
 if (evalue === 1) {
     e1 = select('#e1');
-    e1.style('background-color', 'yellow');
+    e1.style('background-color', 'rgb(122, 198, 122)');
+    e1.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
   
 if (evalue === 2) {
     e2 = select('#e2');
-    e2.style('background-color', 'yellow');
+    e2.style('background-color', 'rgb(122, 198, 122)');
+    e2.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 3) {
     e3 = select('#e3');
-    e3.style('background-color','yellow');
+    e3.style('background-color','rgb(122, 198, 122)');
+    e3.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 4) {
     e4 = select('#e4');
-    e4.style('background-color','yellow');
+    e4.style('background-color','rgb(122, 198, 122)');
+    e4.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 5) {
     e5 = select('#e5');
-    e5.style('background-color','yellow');
+    e5.style('background-color','rgb(122, 198, 122)');
+    e5.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 6) {
     e6 = select('#e6');
-    e6.style('background-color','yellow');
+    e6.style('background-color','rgb(122, 198, 122)');
+    e6.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 7) {
     e7 = select('#e7');
-    e7.style('background','yellow');
+    e7.style('background','rgb(122, 198, 122)');
+    e7.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 8) {
     e8 = select('#e8');
-    e8.style('background','yellow');
+    e8.style('background','rgb(122, 198, 122)');
+    e8.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 9) {
     e9 = select('#e9');
-    e9.style('background','yellow');
+    e9.style('background','rgb(122, 198, 122)');
+    e9.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 10) {
     e_10 = select('#e_10');
-    e_10.style('background','yellow');
+    e_10.style('background','rgb(122, 198, 122)');
+    e_10.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 11) {
     e_11 = select('#e_11');
-    e_11.style('background','yellow');
+    e_11.style('background','rgb(122, 198, 122)');
+    e_11.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (evalue === 12) {
     e_12 = select('#e_12');
-    e_12.style('background','yellow');
+    e_12.style('background','rgb(122, 198, 122)');
+    e_12.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 // f = 5
 
 if (fvalue === 1) {
     f1 = select('#f1');
-    f1.style('background','yellow');
+    f1.style('background','rgb(122, 198, 122)');
+    f1.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (fvalue === 2) {
     f2 = select('#f2');
-    f2.style('background','yellow');
+    f2.style('background','rgb(122, 198, 122)');
+    f2.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (fvalue === 3) {
     f3 = select('#f3');
-    f3.style('background','yellow');
+    f3.style('background','rgb(122, 198, 122)');
+    f3.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (fvalue === 4) {
     f4 = select('#f4');
-    f4.style('background','yellow');
+    f4.style('background','rgb(122, 198, 122)');
+    f4.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (fvalue === 5) {
     f5 = select('#f5');
-    f5.style('background','yellow');
+    f5.style('background','rgb(122, 198, 122)');
+    f5.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 // g = 3
 
 if (gvalue === 1) {
     g1 = select('#g1');
-    g1.style('background','yellow');
+    g1.style('background','rgb(122, 198, 122)');
+    g1.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (gvalue === 2) {
     g2 = select('#g2');
-    g2.style('background','yellow');
+    g2.style('background','rgb(122, 198, 122)');
+    g2.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 if (gvalue === 3) {
     g3 = select('#g3');
-    g3.style('background','yellow');
+    g3.style('background','rgb(122, 198, 122)');
+    g3.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 // c = 1
 
 if (cvalue === 1) {
     c1 = select('#c1');
-    c1.style('background','yellow');
+    c1.style('background','rgb(122, 198, 122)');
+    c1.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 // d = 1
 
 if (dvalue === 1) {
     d1 = select('#d1');
-    d1.style('background','yellow');
+    d1.style('background','rgb(122, 198, 122)');
+    d1.style('boxShadow', '5px 5px 10px rgba(0, 0, 0, 0.5)');
 }
 
 // a = 1
 
 if (avalue === 1) {
     a1 = select('#a1');
-    a1.style('background','red');
+    lucy = select('.lucy');
+    schroeder = select('.schroeder');
+    a1.style('background','#cf7171ff');
+    a1.style('boxShadow', '0px 0px 25px rgba(255, 122, 133, 0.68)');
+    lucy.style('visibility', 'visible');
+    schroeder.style('visibility', 'hidden');
 }
 }
 
 function keyTyped() {
-//   this may also be helpful with keyCode() https://www.toptal.com/developers/keycode
   if (key === 'c') {
       note1.play();
       cvalue += 1;
@@ -180,8 +205,3 @@ function keyTyped() {
   }
 }
 
-// Opt-in: //
-// function typing() {
-//   hint = select('#hint');
-//   hint.html("try typing");
-// }
